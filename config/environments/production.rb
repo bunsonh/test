8fb3221028709d3ffc8797b1de706a68af492408
bunsonh <bunsonh@gmail.com>
2013-12-05 18:43:01 -0500

    Update Gemfile.lock for Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,9 @@ Ruby::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  # ES - Added eager_load
+  config.eager_load = true
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
